dc3d35e06ebe845b1be4d5569946b4d8b6eb4a22
athulsnair08-ops <athulsnair08@gmail.com>
2026-08-29 01:26:16 +0530

    Add files via upload

---
+++ questline-2/counter/script.js
@@ -0,0 +1,21 @@
+let count = 0;
+
+const counter = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
+const resetButton = document.getElementById("reset");
+
+incrementButton.addEventListener("click", function () {
+    count++;
+    counter.textContent = count;
+});
+
+decrementButton.addEventListener("click", function () {
+    count--;
+    counter.textContent = count;
+});
+
+resetButton.addEventListener("click", function () {
+    count = 0;
+    counter.textContent = count;
+});
